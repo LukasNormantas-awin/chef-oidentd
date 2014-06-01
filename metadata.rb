d8a6ff82898a176350fa8dca41fcd998c7be1a43
Lukas Normantas <baciuvys@gmail.com>
2014-06-02 00:24:47 +0100

    RHEL/Centos support

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,12 @@ description      "Installs and configures oidentd"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version          "1.0.0"
 
-%w(debian ubuntu).each do |platform|
+%w(debian ubuntu centos redhat).each do |platform|
   supports platform
+end
+
+%w(yum-epel).each do |dependency_name|
+  depends dependency_name
 end
 
 recipe           "oidentd", "Installs and configures oidentd"
